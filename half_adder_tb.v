77fb0c3c982603610b43b08ed06c85e98259759f
MUTHU RAMAN S <127648597+muthuraman0508@users.noreply.github.com>
2024-02-25 15:41:46 +0530

    Add files via upload

---
+++ half_adder_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+module half_adder_tb;
+	reg a;
+	reg b;
+	wire sum;
+	wire carry;
+	half_adder uut (.a(a),.b(b),.sum(sum),.carry(carry));
+	initial begin
+		a = 0;
+		b = 0;
+		$display("a=%b,b=%b,sum=%b,carry=%b",a,b,sum,carry);
+		#10;
+		a = 0;
+		b = 1;
+		$display("a=%b,b=%b,sum=%b,carry=%b",a,b,sum,carry);
+		#10;
+		a = 1;
+		b = 0;
+		$display("a=%b,b=%b,sum=%b,carry=%b",a,b,sum,carry);
+		#10;
+		a = 1;
+		b = 1;
+		$display("a=%b,b=%b,sum=%b,carry=%b",a,b,sum,carry);
+		#10;
+		$finish;
+	end
+endmodule 
